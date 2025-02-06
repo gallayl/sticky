95578e322ff1072270b133c103779ec82efc56c7
Gallay Lajos <gallay.lajos@gmail.com>
2025-02-06 11:18:29 +0100

    fixed pir action, added debounced

--- src/FeatureRegistry/Features/Pir.h
+++ src/FeatureRegistry/Features/Pir.h
@@ -10,45 +10,104 @@
 
 #define PIR_PIN D6
 
-void IRAM_ATTR  pirMovement() {
-  if (digitalRead(PIR_PIN) == HIGH) {
-    LoggerInstance->Info("PIR movement detected");
-    webSocket->textAll("{\"type\":\"pir\",\"state\":true}");
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    LoggerInstance->Info("PIR movement stopped");
-    webSocket->textAll("{\"type\":\"pir\",\"state\":false}");
-    digitalWrite(LED_BUILTIN, LOW);
+bool getPirState()
+{
+  return digitalRead(PIR_PIN) == HIGH ? true : false;
+}
+
+struct PirStateHistoryEntry
+{
+  bool state;
+  unsigned long epochTime;
+};
+
+#define PIR_HISTORY_SIZE 25
+
+std::vector<PirStateHistoryEntry> pirStateHistory;
+
+bool getDebouncedPirState(unsigned long timeToDebounce)
+{
+  bool currentState = getPirState();
+  if (currentState == true)
+  {
+    return true;
+  }
+
+  unsigned long now = millis();
+
+  for (int i = pirStateHistory.size() - 1; i >= 0; i--)
+  {
+    if (pirStateHistory[i].state == true && now - pirStateHistory[i].epochTime < timeToDebounce)
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void pushToPirStateHistory(bool state)
+{
+  pirStateHistory.push_back({state, millis()});
+  if (pirStateHistory.size() > PIR_HISTORY_SIZE)
+  {
+    pirStateHistory.erase(pirStateHistory.begin());
   }
 }
 
+void IRAM_ATTR pirMovement()
+{
+  if (digitalRead(PIR_PIN) == HIGH)
+  {
+    LoggerInstance->Info("PIR movement detected");
+    webSocket->textAll("{\"type\":\"pir\",\"state\":true}");
+    pushToPirStateHistory(true);
+  }
+  else
+  {
+    LoggerInstance->Info("PIR movement stopped");
+    webSocket->textAll("{\"type\":\"pir\",\"state\":false}");
+    pushToPirStateHistory(false);
+  }
+}
 
-CustomCommand *getPirState = new CustomCommand("getPirState", [](String command) {
+CustomCommand *getPirStateCommand = new CustomCommand("getPirState", [](String command)
+                                                      {
   JsonDocument response = JsonDocument().to<JsonObject>();
-  response["state"] = digitalRead(PIR_PIN) == HIGH ? true : false;
+  response["state"] = getPirState();
   char buffer[64];
   serializeJson(response, buffer);
-  return String(buffer);
-});
+  return String(buffer); });
 
-
-ArRequestHandlerFunction getPirStateAction = [](AsyncWebServerRequest *request) {
-    AsyncJsonResponse * resp = new AsyncJsonResponse();
-    JsonObject response = JsonObject();
-    response["state"] = digitalRead(PIR_PIN);
-    resp->setCode(200);
-    resp->getRoot().set(response);
-    resp->setLength();
-    request->send(resp);
+ArRequestHandlerFunction getPirStateAction = [](AsyncWebServerRequest *request)
+{
+  AsyncJsonResponse *resp = new AsyncJsonResponse();
+  JsonDocument response;
+  response["state"] = getPirState();
+  resp->setCode(200);
+  resp->getRoot().set(response);
+  resp->setLength();
+  request->send(resp);
 };
 
+ArRequestHandlerFunction getDebouncedPirStateAction = [](AsyncWebServerRequest *request)
+{
+  AsyncJsonResponse *resp = new AsyncJsonResponse();
+  JsonDocument response;
+  unsigned long timeToDebounce = request->hasParam("timeToDebounce") ? request->getParam("timeToDebounce")->value().toInt() : 60000; // 1 min by default
+  response["state"] = getDebouncedPirState(timeToDebounce);
+  resp->setCode(200);
+  resp->getRoot().set(response);
+  resp->setLength();
+  request->send(resp);
+};
 
-
-
-Feature* PirFeature = new Feature("pir", []() {
+Feature *PirFeature = new Feature("pir", []()
+                                  {
     attachInterrupt(PIR_PIN, pirMovement, CHANGE);
-    CommandInterpreterInstance->RegisterCommand(*getPirState);
+    CommandInterpreterInstance->RegisterCommand(*getPirStateCommand);
     server.on("/pir", HTTP_GET, getPirStateAction);
 
-  return FeatureState::RUNNING;
-}, [](){});
+    server.on("/pir-debounced", HTTP_GET, getDebouncedPirStateAction);
+
+  return FeatureState::RUNNING; }, []() {});
